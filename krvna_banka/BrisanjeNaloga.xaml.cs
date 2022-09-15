e1df9166540256de51535b8c16dda5f80ea7109c
nikolastrinic <99885789+nikolastrinic@users.noreply.github.com>
2022-09-15 16:24:48 +0200

    GlavniOpcioniMeni gotov, Brisanje  naloga gotovo

---
+++ krvna_banka/BrisanjeNaloga.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using MySql.Data.MySqlClient;
+
+namespace krvna_banka
+{
+    /// <summary>
+    /// Interaction logic for BrisanjeNaloga.xaml
+    /// </summary>
+    public partial class BrisanjeNaloga : Window
+    {
+        public BrisanjeNaloga()
+        {
+            InitializeComponent();
+        }
+
+        string MyConString = "SERVER=127.0.0.1;" + "DATABASE=bankakrvi2;" + "UID=root;" + "PASSWORD=root03102000;";
+        int kojimeni = 0;
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (kojimeni == 0)
+            {
+                var GlavniOpcioniMeni = new GlavniOpcioniMeni();
+                GlavniOpcioniMeni.Show();
+            }
+            else
+            {
+                var MainWindow = new MainWindow();
+                MainWindow.Show();
+            }
+
+        }
+
+        private void ObrisiNalogButton_Click(object sender, RoutedEventArgs e)
+        {
+            string kime = KorisnikInformacije.GetKorisnickoIme<string>("KorisnikI");
+            string ksif = KorisnikInformacije.GetSifra<string>("KorisnikS");
+
+            string username = KorisnickoImeTextBox.Text;
+            string password = SifraTextBox.Text;
+
+
+
+
+            if (KorisnickoImeTextBox.Text.Length == 0 || SifraTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Popunite sva polja!");
+            }
+            else
+            {
+                if (KorisnickoImeTextBox.Text == kime && SifraTextBox.Text == ksif)
+                {
+
+                    string query = "delete from korisnik where KorisnickoIme = '" + KorisnickoImeTextBox.Text + "' and Sifra = '" + SifraTextBox.Text + "'";
+                    MySqlConnection connection = new MySqlConnection(MyConString);
+                    connection.Open();
+
+                    MySqlCommand cmdSel = new MySqlCommand(query, connection);
+                    DataTable dt = new DataTable();
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
+                    da.Fill(dt);
+
+
+                    MessageBox.Show("Nalog Obrisan!");
+                    connection.Close();
+
+                    kojimeni = 1;
+
+                    this.Close();
+
+
+
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Pogresno korisnicko ime ili sifra!");
+                }
+            }
+        }
+    }
+}
--- krvna_banka/GlavniOpcioniMeni.xaml.cs
+++ krvna_banka/GlavniOpcioniMeni.xaml.cs
@@ -23,5 +23,59 @@ namespace krvna_banka
         {
             InitializeComponent();
         }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void RegistrujDonoraButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            var RegistrovanjeDonora = new RegistrovanjeDonora();
+            RegistrovanjeDonora.Show();
+        }
+
+        private void btnRegPacijenta_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrovanjePacijenta prozor = new RegistrovanjePacijenta();
+            this.Visibility = Visibility.Hidden;
+            prozor.Show();
+        }
+
+        private void PregledDonoraButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            var PregledDonora = new PregledDonora();
+            PregledDonora.Show();
+        }
+
+        private void PregledPacijenataButton_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            var PregledPacijenta = new PregledPacijenta();
+            PregledPacijenta.Show();
+        }
+
+        private void btnDonacija_Click(object sender, RoutedEventArgs e)
+        {
+            Donacija prozor = new Donacija();
+            this.Visibility = Visibility.Hidden;
+            prozor.Show();
+        }
+
+        private void ObrisiNalogButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            var BrisanjeNaloga = new BrisanjeNaloga();
+            BrisanjeNaloga.Show();
+        }
+
+        private void IzlogujSeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var MainWindow = new MainWindow();
+            this.Hide();
+            MainWindow.Show();
+        }
     }
 }
