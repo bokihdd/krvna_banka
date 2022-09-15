dab6953bf7c7f4a1a4f3d69039e15d1c7f439f49
bokihdd <brajkov.boris1@gmail.com>
2022-09-15 21:26:23 +0200

    zavrsen pregled donora

--- krvna_banka/PregledDonora.xaml.cs
+++ krvna_banka/PregledDonora.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
 
 namespace krvna_banka
 {
@@ -23,5 +27,29 @@ namespace krvna_banka
         {
             InitializeComponent();
         }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            var GlavniOpcioniMeni = new GlavniOpcioniMeni();
+            GlavniOpcioniMeni.Show();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["mysqlconnection"].ConnectionString;
+            MySqlConnection sqlconn = new MySqlConnection(mainconn);
+            string query = "SELECT donor.Ime,donor.Prezime,krvne_grupe.naziv AS Grupa,donor.KolicinaKrvi AS Kolicina,donor.DatumDoniranja AS Datum FROM donor, krvne_grupe WHERE donor.idKrvneGrupe = krvne_grupe.idKrvneGrupe";
+            sqlconn.Open();
+            MySqlCommand sqlcomm = new MySqlCommand(query, sqlconn);
+            MySqlDataAdapter sda = new MySqlDataAdapter(sqlcomm);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dtGrid.ItemsSource = dt.DefaultView;
+            sqlconn.Close();
+
+        }
+
+        
     }
 }
